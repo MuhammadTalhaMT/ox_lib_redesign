6d6d601de7d4d4f409721da21b601bbc2b52710e
Talha <47274793+MuhammadTalhaMT@users.noreply.github.com>
2024-05-13 17:49:37 +0500

    Add files via upload

---
+++ web/src/theme/index.ts
@@ -0,0 +1,15 @@
+import { MantineThemeOverride } from '@mantine/core';
+
+export const theme: MantineThemeOverride = {
+  fontFamily: 'Inter',
+  shadows: { sm: '1px 1px 3px rgba(0, 0, 0, 0.5)' },
+  components: {
+    Button: {
+      styles: {
+        root: {
+          border: 'none',
+        },
+      },
+    },
+  },
+};
